2375e26d0753313aa8b669854ee389407af0b1e7
dbleall <99196913+dbleall@users.noreply.github.com>
2022-03-04 08:55:40 -0300

    Installing JetStream

---
+++ resources/views/services/social_media.blade.php
@@ -0,0 +1,21 @@
+<x-guest-layout>
+<header class="masthead">
+    <div class="container">
+        <div class="masthead-subheading">Welcome to our Marketing Agency</div>
+        <div class="masthead-heading text-uppercase">The best way to know the future its to create our own.</div>
+        <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell me more</a>
+    </div>
+</header>
+<!-- Services-->
+<section class="page-section" id="social_media">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Brand & Logo</h2>
+            {{-- <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> --}}
+        </div>
+        <div class="row">
+                <h3></h3>
+        </div>
+    </div>
+</section>
+</x-guest-layout>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+
+Route::get('/', function(){
+    return view('home');
+});
+
+Route::get('/brand', function(){
+    return view('services/brand');
+});
+
+Route::get('/webdesign', function(){
+    return view('services/webdesign');
+});
+
+Route::get('/socialmedia', function(){
+    return view('services/social_media');
+});
